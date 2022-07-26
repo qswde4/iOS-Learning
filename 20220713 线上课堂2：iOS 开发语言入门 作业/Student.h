8caebd498983fcd384fbc8672cb35b62cba0fc44
qswde4 <76806402+qswde4@users.noreply.github.com>
2022-07-27 01:40:24 +0800

    Update Student.h

--- 20220713 线上课堂2：iOS 开发语言入门 作业/Student.h
+++ 20220713 线上课堂2：iOS 开发语言入门 作业/Student.h
@@ -1,5 +1,7 @@
 //
 //  Student.h
+//  Student
+//
 //  Created by qswde4 on 2022/07/18.
 //
 
@@ -7,13 +9,16 @@
 #define Student_h
 
 #import <Foundation/Foundation.h>
+#import "CoreLocation/CLLocation.h"
+
+NS_ASSUME_NONNULL_BEGIN
 
 @interface Student : NSObject
-{
-    NSString *name;
-    NSString *major;
-    int age;
-}
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *major;
+@property (nonatomic, assign) NSUInteger age;
+
 @end
 
 #endif
